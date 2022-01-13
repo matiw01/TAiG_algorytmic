b1da62bc4e8e28a52eaacb8f5435636d254420ad
Loloxon <nikodemko@interia.pl>
2022-01-13 19:45:56 +0100

    added import vertex, edge

--- Vertex.py
+++ Vertex.py
@@ -1,4 +1,6 @@
 from Edge import Edge
+
+
 class Vertex:
 
     def __init__(self, idx: int, label: str, out_edges=None, in_edges=None):
--- gui.py
+++ gui.py
@@ -14,12 +14,13 @@ FG_COLOR = "#CCCCCC"
 PROD_BG_COLOR = "#777777"
 FONT = "Courier"
 
+
 def prod_input_window(backend, prod_frame):
     window = tk.Toplevel()
     window.title("Graph rewriting - production input")
     frame = tk.Frame(window, bg=BG_COLOR, width=300, height=500)
     frame.pack(fill=tk.BOTH, expand=True)
-    
+
     left_label = tk.Label(frame, text="Left side of the production", bg=BG_COLOR, fg=FG_COLOR, font=FONT)
     left_label.grid(row=0, column=0, pady=5)
     left_text_box = tk.Text(frame, width=80, height=6, bg=FG_COLOR)
@@ -40,11 +41,11 @@ def prod_input_window(backend, prod_frame):
 
     def add_prod(prod_frame):
         # walidacja
-        left = left_text_box.get("1.0",'end-1c')
-        right = right_text_box.get("1.0",'end-1c')
-        prod = prod_text_box.get("1.0",'end-1c')
+        left = left_text_box.get("1.0", 'end-1c')
+        right = right_text_box.get("1.0", 'end-1c')
+        prod = prod_text_box.get("1.0", 'end-1c')
         backend.add_production(left, "left", right, "right", prod)
-       
+
         # window = tk.Toplevel()
         # window.title("Production added")
         # frame = tk.Frame(window, bg=BG_COLOR, width=300, height=100)
@@ -54,11 +55,12 @@ def prod_input_window(backend, prod_frame):
         # label.pack(fill=tk.BOTH, expand=True)
 
         l_graph, r_graph = backend.productions[-1].get_rl_graphs()
-        add_production_to_list(prod_frame, l_graph, r_graph, prod, len(backend.productions)-1)
+        add_production_to_list(prod_frame, l_graph, r_graph, prod, len(backend.productions) - 1)
 
     s = ttk.Style()
     s.configure("my.TButton", font=(FONT, 12))
-    button = ttk.Button(frame, width=15, text="Add production", style="my.TButton", command=lambda: add_prod(prod_frame))
+    button = ttk.Button(frame, width=15, text="Add production", style="my.TButton",
+                        command=lambda: add_prod(prod_frame))
     button.grid(row=6, column=0, pady=10)
 
 
@@ -76,9 +78,9 @@ def graph_input_window(backend):
 
     def add_graph():
         # walidacja
-        graph = graph_text_box.get("1.0",'end-1c')
+        graph = graph_text_box.get("1.0", 'end-1c')
         backend.add_graph(graph, "graph")
-       
+
         # window = tk.Toplevel()
         # window.title("Graph added")
         # frame = tk.Frame(window, bg=BG_COLOR, width=300, height=100)
@@ -87,18 +89,17 @@ def graph_input_window(backend):
         # label = tk.Label(frame, text="Graph added", bg=BG_COLOR, fg=FG_COLOR, font=(FONT, 18))
         # label.pack(fill=tk.BOTH, expand=True)
 
-
     s = ttk.Style()
     s.configure("my.TButton", font=(FONT, 12))
     button = ttk.Button(frame, width=15, text="Add graph", style="my.TButton", command=add_graph)
     button.grid(row=6, column=0, pady=10)
 
+
 def add_production_to_list(prod_frame, l_graph, r_graph, prod, num):
     frame = tk.Frame(prod_frame, bg=PROD_BG_COLOR)
-    
+
     num_label = tk.Label(frame, text=f"{num}", font=(FONT, 20), bg=PROD_BG_COLOR)
     num_label.grid(row=0, column=0, rowspan=2)
-    
 
     l_graph_img = ImageTk.PhotoImage(data=l_graph.get_graph(PROD_BG_COLOR))
     l_graph_label = tk.Label(frame, image=l_graph_img, bg=PROD_BG_COLOR)
@@ -107,7 +108,7 @@ def add_production_to_list(prod_frame, l_graph, r_graph, prod, num):
 
     arrow_label = tk.Label(frame, text="=>", font=(FONT, 10), bg=PROD_BG_COLOR)
     arrow_label.grid(row=0, column=2)
-    
+
     r_graph_img = ImageTk.PhotoImage(data=r_graph.get_graph(PROD_BG_COLOR))
     r_graph_img.photo = r_graph_img
     r_graph_label = tk.Label(frame, image=r_graph_img, bg=PROD_BG_COLOR)
@@ -119,14 +120,16 @@ def add_production_to_list(prod_frame, l_graph, r_graph, prod, num):
 
     frame.pack(side=tk.TOP, pady=5)
 
+
 def show_graph(show_graph_frame, backend, number):
     global current_graph
     current_graph = number
-    
-    if number < len(backend.graphs): 
+
+    if number < len(backend.graphs):
         graph = backend.graphs[number]
         current_graph = number
-    else: return
+    else:
+        return
 
     graph_data = graph.get_graph(FG_COLOR)
     img = ImageTk.PhotoImage(data=graph_data)
@@ -138,37 +141,40 @@ def show_graph(show_graph_frame, backend, number):
     label.photo = img
     label.pack(fill=tk.BOTH, expand=True)
 
+
 def apply_production(show_graph_frame, backend, prod_num, verticies):
     global current_graph
     graph = backend.graphs[current_graph]
 
     if prod_num < len(backend.productions):
         production = backend.productions[prod_num]
-    else: return
+    else:
+        return
 
     backend.use_production(production, graph, verticies)
     show_graph(show_graph_frame, backend, current_graph)
 
+
 # MAIN WINDOW
 current_graph = 0
+
 
 def main():
     # API FOR BACKEND
     backend = Main()
-
 
     main_window = tk.Tk()
     main_window.title("Graph rewriting - main window")
 
     # LISTING PRODUCTIONS
 
-    prod_list_frame = tk.Frame(main_window, height=HEIGHT, width=WIDTH/3, bg=PROD_BG_COLOR)
+    prod_list_frame = tk.Frame(main_window, height=HEIGHT, width=WIDTH / 3, bg=PROD_BG_COLOR)
     prod_list_frame.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
     # prod_list_frame.propagate(0)
 
     prod_canvas = tk.Canvas(prod_list_frame, bg=PROD_BG_COLOR, bd=0, highlightthickness=0, relief='ridge')
     prod_canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
-    
+
     scrollbar = ttk.Scrollbar(prod_list_frame, orient=tk.VERTICAL)
     scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
     scrollbar.config(command=prod_canvas.yview)
@@ -178,11 +184,11 @@ def main():
     prod_frame = tk.Frame(prod_canvas, bg=PROD_BG_COLOR)
     prod_frame.pack()
 
-    prod_canvas.create_window((0,0), window=prod_frame, anchor="nw")
+    prod_canvas.create_window((0, 0), window=prod_frame, anchor="nw")
 
     # SIDE WITH THE GRAPH
 
-    graph_frame = tk.Frame(main_window, height=HEIGHT, width=2*WIDTH/3, bg=FG_COLOR)
+    graph_frame = tk.Frame(main_window, height=HEIGHT, width=2 * WIDTH / 3, bg=FG_COLOR)
     graph_frame.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True)
     graph_frame.propagate(0)
 
@@ -194,15 +200,17 @@ def main():
     window_buttons.pack(side=tk.TOP, fill=tk.X)
     window_buttons.propagate(0)
 
-    graph_input_btn = ttk.Button(window_buttons, text="Graph input", width=20, command=lambda: graph_input_window(backend), style="my.TButton")
+    graph_input_btn = ttk.Button(window_buttons, text="Graph input", width=20,
+                                 command=lambda: graph_input_window(backend), style="my.TButton")
     graph_input_btn.pack(side=tk.RIGHT, padx=5, pady=5)
 
-    prod_input_btn = ttk.Button(window_buttons, text="Production input", width=20, command=lambda: prod_input_window(backend, prod_frame), style="my.TButton")
+    prod_input_btn = ttk.Button(window_buttons, text="Production input", width=20,
+                                command=lambda: prod_input_window(backend, prod_frame), style="my.TButton")
     prod_input_btn.pack(side=tk.RIGHT)
 
     # SHOW GRAPH FRAME
     show_graph_frame = tk.Frame(graph_frame, bg=FG_COLOR)
-    show_graph_frame.pack(side=tk.TOP, fill=tk.BOTH,  expand=True)
+    show_graph_frame.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 
     # CHOOSE GRAPH TO SHOW AND APPLY PRODUCTION
     apply_frame = tk.Frame(graph_frame, height=80, bg=BG_COLOR)
@@ -216,9 +224,9 @@ def main():
 
     prod_label = tk.Label(change_graph_frame, text="Choose graph", bg=BG_COLOR, fg=FG_COLOR, font=FONT)
     prod_label.grid(row=0, column=0, columnspan=3, pady=5)
-    
-    change_button = ttk.Button(change_graph_frame, text="Change to", width=10, style="my.TButton", 
-        command=lambda: show_graph(show_graph_frame, backend, int(change_entry.get())))
+
+    change_button = ttk.Button(change_graph_frame, text="Change to", width=10, style="my.TButton",
+                               command=lambda: show_graph(show_graph_frame, backend, int(change_entry.get())))
     change_button.grid(row=1, column=0, padx=5, pady=5)
     change_entry = tk.Entry(change_graph_frame, bg=FG_COLOR, width=5)
     change_entry.insert(tk.INSERT, "0")
@@ -231,9 +239,8 @@ def main():
     reverse_label = tk.Label(reverse_frame, text="Reverse graph to previous state", bg=BG_COLOR, fg=FG_COLOR, font=FONT)
     reverse_label.grid(row=0, column=0, pady=5)
     reverse_button = ttk.Button(reverse_frame, text="Reverse", width=10, style="my.TButton",
-        command=lambda: 1) #TODO
+                                command=lambda: 1)  # TODO
     reverse_button.grid(row=1, column=0, pady=5)
-
 
     # APPLY PRODUCTION
     apply_prod_frame = tk.Frame(apply_frame, bg=BG_COLOR)
@@ -243,9 +250,10 @@ def main():
     prod_label = tk.Label(apply_prod_frame, text="Apply production", bg=BG_COLOR, fg=FG_COLOR, font=FONT)
     prod_label.grid(row=0, column=0, columnspan=3, pady=5)
 
-    apply_button = ttk.Button(apply_prod_frame, text="Apply", width=5, style="my.TButton", 
-        command=lambda: apply_production(show_graph_frame, backend, int(entry_prod.get()), [int(x) for x in entry_graph.get().split(" ")]))
-    apply_button.grid(row=1, column=0, padx=5, pady=5) 
+    apply_button = ttk.Button(apply_prod_frame, text="Apply", width=5, style="my.TButton",
+                              command=lambda: apply_production(show_graph_frame, backend, int(entry_prod.get()),
+                                                               [int(x) for x in entry_graph.get().split(" ")]))
+    apply_button.grid(row=1, column=0, padx=5, pady=5)
 
     entry_prod = tk.Entry(apply_prod_frame, bg=FG_COLOR, width=5)
     entry_prod.grid(row=1, column=1, padx=5, pady=5)
@@ -253,7 +261,6 @@ def main():
     entry_graph = tk.Entry(apply_prod_frame, bg=FG_COLOR, width=25)
     entry_graph.insert(tk.INSERT, "Graph indicies here")
     entry_graph.grid(row=1, column=2, padx=5, pady=5)
-
 
     main_window.mainloop()
 
