013a1a62ab238772a697aa17db9d345b210e3400
Tobiasz Szulc <szulc.tobiasz@gmail.com>
2022-01-13 09:23:16 +0100

    pruned some warnings, improved typing a bit, started debugging use_production

--- Production.py
+++ Production.py
@@ -1,9 +1,33 @@
 # expected_format = '[([(s1, out, 1, 2, 3), (ai, out, 1, 2)], [(0, A, M, in), (0, A, N, out)]), ' \
 #     '([(s1, out, 1, 2, 3), (ai, out, 1, 4)], [(0, A, M, in), (0, A, N, out)]), ' \
 #     '([(s1, out, 1, 2, 5), (ai, out, 1, 6)], [(0, A, M, in), (0, A, N, out)])]'
+from enum import Enum
+from typing import List, Tuple
+
+from Graph import Graph
+
+
+class Direction(Enum):
+    IN = 0
+    OUT = 1
+
+    @staticmethod
+    def from_str(s: str):
+        if s == 'in':
+            return Direction.IN
+        if s == 'out':
+            return Direction.OUT
+        raise ValueError(f"{s} is neither 'in' nor 'out"'')
+
+
+Removed_edge = Tuple[str, Direction, int]
+
+Added_edge = Tuple[str, str, str, Direction]
+
 
 class Production:
-    def __init__(self, left_graph, right_graph, attachment):
+
+    def __init__(self, left_graph: Graph, right_graph: Graph, attachment: List[Removed_edge | Added_edge]):
         self.operations = []
         self.left_graph = left_graph
         self.right_graph = right_graph
@@ -13,7 +37,7 @@ class Production:
         return self.left_graph, self.right_graph
 
     @staticmethod
-    def if_input_is_valid(input_data) -> bool:
+    def if_input_is_valid(input_data: str) -> bool:
         S = input_data.replace('\n', '\n ')
         S = S.split(' ')
         print(S)
@@ -36,7 +60,7 @@ class Production:
     # [[[edge_label, direction, index_left], [ver_right_label, ver_g_label, edge_label, direction]],
     # [[edge_label, direction, index_left], [ver_right_label, ver_g_label, edge_label, direction]]]
     @staticmethod
-    def parse(input_data: str):
+    def parse(input_data: str) -> List[Removed_edge | Added_edge]:
         S = input_data[:-1]
         S = S.replace('\n', ' ')
         S = S.split('; ')
@@ -51,7 +75,11 @@ class Production:
                 idxV += 1
             V[idxV].append(S[idx])
             idx += 1
-        return V
+        if len(V) == 0:
+            return []
+        return [[(operation[0][0], Direction.from_str(operation[0][1]), int(operation[0][2]))] + \
+               [(added_edge_desc[0], added_edge_desc[1], added_edge_desc[2], Direction.from_str(added_edge_desc[3]))
+                for added_edge_desc in operation[1:]] for operation in V]
 
 # if __name__ == '__main__':
 #     Prod = Production()
