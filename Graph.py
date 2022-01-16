39464c2db31ee8170283afa88df26a56f08159fb
Falon <falon4522@gmail.com>
2022-01-16 01:34:52 +0100

    Some code refractoring

--- Graph.py
+++ Graph.py
@@ -6,7 +6,7 @@ from Edge import Edge
 from Vertex import Vertex
 
 
-# assuming matrixGraph is matrix where each row is list of outgoing edges and collumn is list of  ingoing edges
+# assuming matrixGraph is matrix where each row is list of outgoing edges and column is list of ingoing edges
 # and labels is list of labels so labels[i] is label for vertex created of matrixGraph[i]
 class Graph:
     def __init__(self, vertices: List[Tuple[int, str]], edges: List[Tuple[int, int, str]], name: str):
@@ -22,134 +22,149 @@ class Graph:
 
     @staticmethod
     def create(self, vertices: List[Tuple[int, str]], edges: List[Tuple[int, int, str]]):
-        self.verticesDict = {}
-        self.labelDict = {}
+        self.vertices_dict = {}
+        self.label_dict = {}
         for idx, label in vertices:
-            self.verticesDict[idx] = Vertex(idx, label)
-            self.labelDict[label] = []
+            self.vertices_dict[idx] = Vertex(idx, label)
+            self.label_dict[label] = []
 
         for idx, label in vertices:
-            self.labelDict[label].append(self.verticesDict[idx])
+            self.label_dict[label].append(self.vertices_dict[idx])
 
         for out_vertex, in_vertex, label in edges:
-            self.verticesDict[out_vertex].add_out_edge(Edge(out_vertex, in_vertex, label))
-            self.verticesDict[in_vertex].add_in_edge(Edge(out_vertex, in_vertex, label))
+            self.vertices_dict[out_vertex].add_out_edge(Edge(out_vertex, in_vertex, label))
+            self.vertices_dict[in_vertex].add_in_edge(Edge(out_vertex, in_vertex, label))
 
     def __str__(self):
-        tmp = "verticies: "
-        for i in self.verticesDict:
-            tmp += str(self.verticesDict[i])
+        tmp = "vertices: "
+        for i in self.vertices_dict:
+            tmp += str(self.vertices_dict[i])
         tmp += "labels: "
-        tmp += str(self.labelDict)
+        tmp += str(self.label_dict)
         return tmp
 
     def get_graph(self, bg_color):
         g = graphviz.Digraph(self.name, format='png')
         g.attr(rankdir='LR', size='10', bgcolor=bg_color)
-        for i in self.verticesDict:
-            g.node(str(i), label=f"{self.verticesDict[i].label},{self.verticesDict[i].idx}")
-            for j in self.verticesDict[i].out_edges:
+        for i in self.vertices_dict:
+            g.node(str(i), label=f"{self.vertices_dict[i].label},{self.vertices_dict[i].idx}")
+            for j in self.vertices_dict[i].out_edges:
                 g.edge(str(i), str(j.in_vertex), label=j.label)
         return g.pipe(format='png')
 
     @staticmethod
-    def if_input_is_valid(input_data: str) -> bool:
+    def is_input_valid(input_data: str) -> bool:
         if "\n" not in input_data:
             return False
-        S = input_data.replace('\n', '\n ')
-        S = S.split(' ')
+        s = input_data.replace('\n', '\n ')
+        s = s.split(' ')
         i = 0
-        S1 = []
-        S2 = []
+        s1 = []
+        s2 = []
         while True:
-            S1.append(S[i])
-            if S[i][-1] == '\n':
+            s1.append(s[i])
+            if s[i][-1] == '\n':
                 i += 1
                 break
             i += 1
-        while i < len(S):
-            S2.append(S[i])
+        while i < len(s):
+            s2.append(s[i])
             i += 1
-        S = input_data.replace('\n', '\n ')
-        S = S.split(' ')
+        s = input_data.replace('\n', '\n ')
+        s = s.split(' ')
         flag = True
-        if len(S1) % 2 != 0:
+        if len(s1) % 2 != 0:
             return False
-        if len(S2) % 3 != 0:
+        if len(s2) % 3 != 0:
             return False
-        for i in range(len(S1)):
-            if (i + 1) % 2 != 0 and S1[i][-1] != ',':
+        for i in range(len(s1)):
+            if (i + 1) % 2 != 0 and s1[i][-1] != ',':
                 flag = False
                 break
-            if (i + 1) % 2 == 0 and i < len(S1) - 2 and S[i][-1] != ";":
+            if (i + 1) % 2 == 0 and i < len(s1) - 2 and s[i][-1] != ";":
                 print(i)
                 flag = False
                 break
-            if i == len(S1) - 1 and S[i][-1] != "\n":
+            if i == len(s1) - 1 and s[i][-1] != "\n":
                 flag = False
                 break
         if not flag:
             return flag
-        for i in range(len(S2)):
-            if (i + 1) % 3 != 0 and S2[i][-1] != ',':
+        for i in range(len(s2)):
+            if (i + 1) % 3 != 0 and s2[i][-1] != ',':
                 flag = False
                 break
-            if (i + 1) % 3 == 0 and i < len(S2) - 1 and S2[i][-1] != ";":
+            if (i + 1) % 3 == 0 and i < len(s2) - 1 and s2[i][-1] != ";":
                 flag = False
                 break
         return flag
 
-    @staticmethod  # input_data "index, label\n index1, index2, label_e;"
+    @staticmethod
     def parse(input_data: str) -> Tuple[List[Tuple[int, str]], List[Tuple[int, int, str]]]:
-        # return [[(vertex_index, vertex_label)],[(vertex1_index, vertex2_index, edge_label)]]
-        S = input_data.replace('\n', '\n ')
-        S = S.split(' ')
+        """ Parses graph
+
+        :param input_data:  "index, label\n index1, index2, label_e;"
+        :return: [[(vertex_index, vertex_label)],[(vertex1_index, vertex2_index, edge_label)]]
+        """
+        s = input_data.replace('\n', '\n ')
+        s = s.split(' ')
         i = 0
-        S1 = []
-        S2 = []
+        s1 = []
+        s2 = []
         while True:
-            if i >= len(S):
-                return Graph.parseVertexes(S1), []
-            if S[i][-1] == '\n':
-                S1.append(S[i][:-1])
+            if i >= len(s):
+                return Graph.parse_vertices(s1), []
+            if s[i][-1] == '\n':
+                s1.append(s[i][:-1])
                 i += 1
                 break
-            S1.append(S[i])
+            s1.append(s[i])
             i += 1
-        while i < len(S):
-            S2.append(S[i])
+        while i < len(s):
+            s2.append(s[i])
             i += 1
-        return Graph.parseVertexes(S1), Graph.parseEdges(S2)
+        return Graph.parse_vertices(s1), Graph.parse_edges(s2)
 
-    @staticmethod  # input_data "index, label;"
-    def parseVertexes(s: List[str]) -> List[Tuple[int, str]]:  # return [(vertex_index, vertex_label)]
-        return [(int(s[i][:-1]), s[i + 1][:-1]) for i in range(0, len(s), 2)]
+    @staticmethod
+    def parse_vertices(input_data: List[str]) -> List[Tuple[int, str]]:
+        """ Parses vertices
 
-    @staticmethod  # input_data "index1, index2, label_e;"
-    def parseEdges(s: List[str]) -> List[Tuple[int, int, str]]:  # return [(vertex1_index, vertex2_index, edge_label)]
-        return [(int(s[i][:-1]), int(s[i + 1][:-1]), s[i + 2][:-1]) for i in range(0, len(s), 3)]
+        :param input_data: "index, label;"
+        :return: [(vertex_index, vertex_label)]
+        """
+        return [(int(input_data[i][:-1]), input_data[i + 1][:-1]) for i in range(0, len(input_data), 2)]
+
+    @staticmethod
+    def parse_edges(input_data: List[str]) -> List[Tuple[int, int, str]]:
+        """ Parses edges
+
+
+        :param input_data: "index1, index2, label_e;"
+        :return: [(vertex1_index, vertex2_index, edge_label)]
+        """
+        return [(int(input_data[i][:-1]), int(input_data[i + 1][:-1]), input_data[i + 2][:-1]) for i in range(0, len(input_data), 3)]
 
     def add_vertex(self, vertex):
-        self.verticesDict[vertex.idx] = vertex
-        if not vertex.label in self.labelDict:
-            self.labelDict[vertex.label] = []
-        self.labelDict[vertex.label].append(self.verticesDict[vertex.idx])
+        self.vertices_dict[vertex.idx] = vertex
+        if vertex.label not in self.label_dict:
+            self.label_dict[vertex.label] = []
+        self.label_dict[vertex.label].append(self.vertices_dict[vertex.idx])
 
     def add_edge(self, edge):
-        self.verticesDict[edge.out_vertex].add_out_edge(edge)
-        self.verticesDict[edge.in_vertex].add_in_edge(edge)
+        self.vertices_dict[edge.out_vertex].add_out_edge(edge)
+        self.vertices_dict[edge.in_vertex].add_in_edge(edge)
 
     def remove_edge(self, edge):
-        self.verticesDict[edge.out_vertex].remove_out_edge(edge)
-        self.verticesDict[edge.in_vertex].remove_in_edge(edge)
+        self.vertices_dict[edge.out_vertex].remove_out_edge(edge)
+        self.vertices_dict[edge.in_vertex].remove_in_edge(edge)
 
     def remove_vertex(self, vertex_id):
-        vertex = self.verticesDict[vertex_id]
+        vertex = self.vertices_dict[vertex_id]
         for edge in vertex.out_edges:
-            self.verticesDict[edge.out_vertex].remove_in_edge(edge)
+            self.vertices_dict[edge.out_vertex].remove_in_edge(edge)
 
         for edge in vertex.in_edges:
-            self.verticesDict[edge.in_vertex].remove_out_edge(edge)
+            self.vertices_dict[edge.in_vertex].remove_out_edge(edge)
 
-        self.labelDict[vertex.label].remove(vertex)
-        del self.verticesDict[vertex_id]
+        self.label_dict[vertex.label].remove(vertex)
+        del self.vertices_dict[vertex_id]
--- main.py
+++ main.py
@@ -33,58 +33,57 @@ class Main:
         if len(self.productions) > idx >= 0:
             self.productions[idx].draw()
 
-    def use_production(self, production: Production, graph: Graph, pointed_vertexes: List[int]):
+    def use_production(self, production: Production, graph: Graph, pointed_vertices: List[int]):
         graph.store()
 
         left_side_to_graph_vertex_id_map = dict()
-        for i in range(0, len(pointed_vertexes), 2):
-            left_side_to_graph_vertex_id_map[pointed_vertexes[i]] = pointed_vertexes[i + 1]
+        for i in range(0, len(pointed_vertices), 2):
+            left_side_to_graph_vertex_id_map[pointed_vertices[i]] = pointed_vertices[i + 1]
         indexes_of_vertices_from_left_side_in_graph = set(left_side_to_graph_vertex_id_map.values())
 
         right_side_to_graph_vertex_id_map = dict()
         indexes_of_vertices_from_right_side_in_graph = set()
 
-        next_idx = max(graph.verticesDict.keys()) + 1
+        next_idx = max(graph.vertices_dict.keys()) + 1
 
-        for idx in production.right_graph.verticesDict:
-            if idx in production.left_graph.verticesDict:
+        for idx in production.right_graph.vertices_dict:
+            if idx in production.left_graph.vertices_dict:
                 right_side_to_graph_vertex_id_map[idx] = left_side_to_graph_vertex_id_map[idx]
                 indexes_of_vertices_from_right_side_in_graph.add(left_side_to_graph_vertex_id_map[idx])
 
-        # usuwanie wierzchołków, których nie ma w prawej stronie
-        for idx in production.left_graph.verticesDict:
-            if not idx in production.right_graph.verticesDict:
+        # delete vertices, that are absent on right side
+        for idx in production.left_graph.vertices_dict:
+            if idx not in production.right_graph.vertices_dict:
                 graph.remove_vertex(left_side_to_graph_vertex_id_map[idx])
 
-        # dodawanie wierzchołków, których nie ma w lewej stronie
-        for idx in production.right_graph.verticesDict:
-            if not idx in production.left_graph.verticesDict:
+        # add vertices, that are absent on left side
+        for idx in production.right_graph.vertices_dict:
+            if idx not in production.left_graph.vertices_dict:
                 new_idx = next_idx
                 next_idx += 1
                 right_side_to_graph_vertex_id_map[idx] = new_idx
                 indexes_of_vertices_from_right_side_in_graph.add(new_idx)
-                vertex = Vertex(new_idx, production.right_graph.verticesDict[idx].label)
+                vertex = Vertex(new_idx, production.right_graph.vertices_dict[idx].label)
                 graph.add_vertex(vertex)
 
-        left_side_edges = [edge for vertex in production.left_graph.verticesDict.values() for edge in vertex.in_edges]
-        right_side_edges = [edge for vertex in production.right_graph.verticesDict.values() for edge in vertex.in_edges]
+        left_side_edges = [edge for vertex in production.left_graph.vertices_dict.values() for edge in vertex.in_edges]
+        right_side_edges = [edge for vertex in production.right_graph.vertices_dict.values() for edge in vertex.in_edges]
 
-
-        # usuwanie krawędzi występujących w lewej stronie
+        # delete vertices, that are present on left side
         for edge in left_side_edges:
             g_edge = Edge(left_side_to_graph_vertex_id_map[edge.out_vertex],
                           left_side_to_graph_vertex_id_map[edge.in_vertex],
                           edge.label)
             graph.remove_edge(g_edge)
 
-        # dodawanie krawędzi występujących w prawej stronie
+        # add vertices, that are present on right side
         for edge in right_side_edges:
             g_edge = Edge(right_side_to_graph_vertex_id_map[edge.out_vertex],
                           right_side_to_graph_vertex_id_map[edge.in_vertex],
                           edge.label)
             graph.add_edge(g_edge)
 
-        # wykonywanie operacji dodawania
+        # do operation, that adds edges
         for operation in production.attachment:
 
             graph_in_idx = left_side_to_graph_vertex_id_map[operation[0][2]]
@@ -93,23 +92,23 @@ class Main:
 
             added_edges = operation[1:]
 
-            left_side_to_graph_edges_to_process = graph.verticesDict[graph_in_idx].in_edges if direction == 'in' else \
-                graph.verticesDict[graph_in_idx].out_edges
+            left_side_to_graph_edges_to_process = graph.vertices_dict[graph_in_idx].in_edges if direction == 'in' else \
+                graph.vertices_dict[graph_in_idx].out_edges
             left_side_to_graph_edges_to_process = list(
                 filter(lambda x: x.label == label, left_side_to_graph_edges_to_process))
 
             for processed_edge in left_side_to_graph_edges_to_process:
                 other_vertex_id = processed_edge.in_vertex if direction == 'out' else processed_edge.out_vertex
                 if other_vertex_id in indexes_of_vertices_from_right_side_in_graph:
-                    continue # krawędź musi być do wierzchołka z poza 'zmapowanej' lewej strony
+                    continue  # the edge must be into a vertex outside of 'mapped' left side
 
                 graph.remove_edge(processed_edge)
 
                 for added_edge in added_edges:
 
                     right_vertices = [right_side_to_graph_vertex_id_map[v.idx] for v in
-                                      production.right_graph.labelDict[added_edge[0]]]
-                    if graph.verticesDict[other_vertex_id].label != added_edge[1]:
+                                      production.right_graph.label_dict[added_edge[0]]]
+                    if graph.vertices_dict[other_vertex_id].label != added_edge[1]:
                         continue
                     # add edges between all matching vertices
                     for right_vertex_id in right_vertices:
@@ -119,7 +118,7 @@ class Main:
                             graph.add_edge(Edge(other_vertex_id, right_vertex_id, added_edge[2]))
 
 
-# format wejścia transformacji osadzenia:
+# Embedded transform input format:
 # """edge_label, direction, index_left; ver_right_label, ver_g_label, edge_label, direction;
 # edge_label, direction, index_left; ver_right_label, ver_g_label, edge_label, direction;"""
 A1 = """s, out, 1; A, N, s, out; N, N, -n, in; N, N, n, in;"""
@@ -147,23 +146,23 @@ g_r3 = """1, A; 2, E;
 g_l4 = """1, A;"""
 g_r4 = """1, A; 2, I;
 1, 2, ai;"""
-grafs = [(g_l1, "g_l1", g_r1, "g_r1"),
-         (g_l2, "g_l2", g_r2, "g_r2"),
-         (g_l3, "g_l3", g_r3, "g_r3"),
-         (g_l4, "g_l4", g_r4, "g_r4"),
-         ]
+graphs = [(g_l1, "g_l1", g_r1, "g_r1"),
+          (g_l2, "g_l2", g_r2, "g_r2"),
+          (g_l3, "g_l3", g_r3, "g_r3"),
+          (g_l4, "g_l4", g_r4, "g_r4"),
+          ]
 
-osadzenia = [A1, A2, A3, A4]
+embedment = [A1, A2, A3, A4]
 
 
-def default(M=None):
+def default(M = None):
     if M is None:
         M = Main()
-    for i in range(len(grafs)):
-        M.add_production(grafs[i][0], grafs[i][1], grafs[i][2], grafs[i][3], osadzenia[i])
-    for i in range(len(grafs)):
+    for i in range(len(graphs)):
+        M.add_production(graphs[i][0], graphs[i][1], graphs[i][2], graphs[i][3], embedment[i])
+    for i in range(len(graphs)):
         for j in range(2):
-            M.add_graph(grafs[i][j * 2], grafs[i][j * 2 + 1])
+            M.add_graph(graphs[i][j * 2], graphs[i][j * 2 + 1])
 
     # for i in range(len(M.productions)):
     #     M.show_production(i)
